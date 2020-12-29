c30ae9386d5ecbe666641eadcf544d01ec13a0c0
diBasch <barbash.dv@gmail.com>
2020-12-29 14:58:29 +0300

    simple animation

--- App.js
+++ App.js
@@ -1,114 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { Component, useEffect } from 'react'
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
-} from 'react-native';
+  View,
+  StyleSheet,
+  Platform,
+  Animated,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const HEADER_MIN_HEIGHT = 50
+const HEADER_MAX_HEIGHT = 200
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.scrollYAnimatedValue = new Animated.Value(0)
+
+    this.array = []
+  }
+  // Похож на componentDidMount и componentDidUpdate:
+  // useEffect(() => {
+  //   // Обновляем название докуммента, используя API браузера
+  //   for (var i = 1; i <= 75; i++) {
+  //     this.array.push(i)
+  //   }
+  // });
+  UNSAFE_componentWillMount() {
+    for (var i = 1; i <= 75; i++) {
+      this.array.push(i)
+    }
+  }
+
+  render() {
+    const headerHeight = this.scrollYAnimatedValue.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp',
+    })
+
+    const headerBackgroundColor = this.scrollYAnimatedValue.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: ['#e91e63', 'grey'],
+      extrapolate: 'clamp',
+    })
+
+    return (
+      <SafeAreaView style={styles.container}>
         <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+          contentContainerStyle={{ paddingTop: HEADER_MAX_HEIGHT }}
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: { contentOffset: { y: this.scrollYAnimatedValue } },
+            },
+          ])}
+        >
+          {this.array.map((item, key) => (
+            <View key={key} style={styles.item}>
+              <Text style={styles.itemText}>Row No : {item}</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+          ))}
         </ScrollView>
+
+        <Animated.View
+          style={[
+            styles.animatedHeaderContainer,
+            { height: headerHeight, backgroundColor: headerBackgroundColor },
+          ]}
+        >
+          <Text style={styles.headerText}>Animated Header</Text>
+        </Animated.View>
       </SafeAreaView>
-    </>
-  );
-};
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  engine: {
+  animatedHeaderContainer: {
     position: 'absolute',
+    top: Platform.OS == 'ios' ? 0 : 0,
+    left: 0,
     right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  headerText: {
+    color: 'white',
+    fontSize: 22,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  item: {
+    backgroundColor: '#ff9e80',
+    marginVertical: 5,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  itemText: {
+    color: 'black',
+    fontSize: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+})
